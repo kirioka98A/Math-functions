153e445e73d04ffd0546ec395fb86f082e51dece
kirioka98A <95756837+kirioka98A@users.noreply.github.com>
2022-03-02 22:40:43 +0300

    Update division-eucledienne.js

--- division-eucledienne.js
+++ division-eucledienne.js
@@ -1,4 +1,4 @@
-//on applique ici la division d'Euclude pour trouver de pgcd d'un nombre
+//on applique ici la division d'Eclude pour trouver de pgcd d'un nombre
 //ceci est en mode réccursif
 function pgcd(input) {
     //on définit la condition d'arret :
@@ -6,7 +6,7 @@ function pgcd(input) {
     // on return le nombre numéro deux car c'est à ce momment là le pgcd
     if (input[0] % input[1] == 0)
         return input[1]
-    //sinon, of réappelle la fonction pgct avec une tableau comme paramètre
+    //sinon, on réappelle la fonction pgct avec une tableau comme paramètre
     //première élément => le second nombre
     //second élément => le reste de la division du premier nombre par le second
     return pgcd([input[1], (input[0] % input[1])])
